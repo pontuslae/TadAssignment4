76d2dbd15987eca7745c7b0c8484117948892fcc
Sebastian Fransson <gusfransse@student.gu.se>
2017-03-13 10:44:55 +0100

    Frame class add

---
+++ metroid/src/Frame.java
@@ -0,0 +1,262 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class Frame extends Applet {
+
+    /**
+     * Variable section
+     */
+
+    // Counter and total used to track the loading of the sound clips.
+    int clipTotal   = 0;
+    int clipsLoaded = 0;
+
+
+    // Copyright information.
+    String copyName = "Asteroids";
+    String copyVers = "Version 1.3";
+    String copyInfo = "Copyright 1998-2001 by Mike Hall";
+    String copyLink = "http://www.brainjar.com";
+    String copyText = copyName + '\n' + copyVers + '\n'
+            + copyInfo + '\n' + copyLink;
+
+    // Constants
+    static final int DELAY = 20;             // Milliseconds between screen and
+    static final int FPS   =                 // the resulting frame rate.
+            Math.round(1000 / DELAY);
+
+
+    static final int MAX_SHOTS =  8;          // Maximum number of sprites
+    static final int MAX_ROCKS =  8;          // for photons, asteroids and
+    static final int MAX_SCRAP = 40;          // explosions.
+    static final int SCRAP_COUNT  = 2 * FPS;  // Timer counter starting values
+    static final int HYPER_COUNT  = 3 * FPS;  // calculated using number of
+    static final int MISSLE_COUNT = 4 * FPS;  // seconds x frames per second.
+    static final int STORM_PAUSE  = 2 * FPS;
+
+    // Ship data.
+    int shipsLeft;       // Number of ships left in game, including current one.
+    int shipCounter;     // Timer counter for ship explosion.
+    int hyperCounter;    // Timer counter for hyperspace.
+
+    // Flags for game state and options.
+    boolean loaded = false;
+    boolean paused;
+    boolean playing;
+    boolean sound;
+    boolean detail;
+
+    // Missle data.
+    int missleCounter;    // Counter for life of missle.
+
+
+    // Sprite objects.
+    AsteroidsSprite   ship;
+    AsteroidsSprite   fwdThruster, revThruster;
+    AsteroidsSprite   ufo;
+    AsteroidsSprite   missle;
+    AsteroidsSprite[] photons    = new AsteroidsSprite[MAX_SHOTS];
+    AsteroidsSprite[] asteroids  = new AsteroidsSprite[MAX_ROCKS];
+    AsteroidsSprite[] explosions = new AsteroidsSprite[MAX_SCRAP];
+
+    // Off screen image.
+    Dimension offDimension;
+    Image     offImage;
+    Graphics  offGraphics;
+
+    // Background stars.
+    int     numStars;
+    Point[] stars;
+
+    // Data for the screen font.
+    Font font      = new Font("Helvetica", Font.BOLD, 12);
+    FontMetrics fm = getFontMetrics(font);
+    int fontWidth  = fm.getMaxAdvance();
+    int fontHeight = fm.getHeight();
+
+    // Explosion data.
+    int[] explosionCounter = new int[MAX_SCRAP];  // Time counters for explosions.
+    int   explosionIndex;                         // Next available explosion sprite.
+
+    // Game data.
+    int score;
+    int highScore;
+    int newShipScore;
+    int newUfoScore;
+
+
+
+
+    /**
+     * Method Section
+     */
+
+
+
+    public void paint(Graphics g) {
+        Dimension d = getSize();
+        int i;
+        int c;
+        String s;
+        int w, h;
+        int x, y;
+
+        // Create the off screen graphics context, if no good one exists.
+
+        if (offGraphics == null || d.width != offDimension.width || d.height != offDimension.height) {
+            offDimension = d;
+            offImage = createImage(d.width, d.height);
+            offGraphics = offImage.getGraphics();
+        }
+
+        // Fill in background and stars.
+
+        offGraphics.setColor(Color.black);
+        offGraphics.fillRect(0, 0, d.width, d.height);
+        if (detail) {
+            offGraphics.setColor(Color.white);
+            for (i = 0; i < numStars; i++)
+                offGraphics.drawLine(stars[i].x, stars[i].y, stars[i].x, stars[i].y);
+        }
+
+        // Draw photon bullets.
+
+        offGraphics.setColor(Color.white);
+        for (i = 0; i < MAX_SHOTS; i++)
+            if (photons[i].active)
+                offGraphics.drawPolygon(photons[i].sprite);
+
+        // Draw the guided missle, counter is used to quickly fade color to black
+        // when near expiration.
+
+        c = Math.min(missleCounter * 24, 255);
+        offGraphics.setColor(new Color(c, c, c));
+        if (missle.active) {
+            offGraphics.drawPolygon(missle.sprite);
+            offGraphics.drawLine(missle.sprite.xpoints[missle.sprite.npoints - 1], missle.sprite.ypoints[missle.sprite.npoints - 1],
+                    missle.sprite.xpoints[0], missle.sprite.ypoints[0]);
+        }
+
+        // Draw the asteroids.
+
+        for (i = 0; i < MAX_ROCKS; i++)
+            if (asteroids[i].active) {
+                if (detail) {
+                    offGraphics.setColor(Color.black);
+                    offGraphics.fillPolygon(asteroids[i].sprite);
+                }
+                offGraphics.setColor(Color.white);
+                offGraphics.drawPolygon(asteroids[i].sprite);
+                offGraphics.drawLine(asteroids[i].sprite.xpoints[asteroids[i].sprite.npoints - 1], asteroids[i].sprite.ypoints[asteroids[i].sprite.npoints - 1],
+                        asteroids[i].sprite.xpoints[0], asteroids[i].sprite.ypoints[0]);
+            }
+
+        // Draw the flying saucer.
+
+        if (ufo.active) {
+            if (detail) {
+                offGraphics.setColor(Color.black);
+                offGraphics.fillPolygon(ufo.sprite);
+            }
+            offGraphics.setColor(Color.white);
+            offGraphics.drawPolygon(ufo.sprite);
+            offGraphics.drawLine(ufo.sprite.xpoints[ufo.sprite.npoints - 1], ufo.sprite.ypoints[ufo.sprite.npoints - 1],
+                    ufo.sprite.xpoints[0], ufo.sprite.ypoints[0]);
+        }
+
+        // Draw the ship, counter is used to fade color to white on hyperspace.
+
+        c = 255 - (255 / HYPER_COUNT) * hyperCounter;
+        if (ship.active) {
+            if (detail && hyperCounter == 0) {
+                offGraphics.setColor(Color.black);
+                offGraphics.fillPolygon(ship.sprite);
+            }
+            offGraphics.setColor(new Color(c, c, c));
+            offGraphics.drawPolygon(ship.sprite);
+            offGraphics.drawLine(ship.sprite.xpoints[ship.sprite.npoints - 1], ship.sprite.ypoints[ship.sprite.npoints - 1],
+                    ship.sprite.xpoints[0], ship.sprite.ypoints[0]);
+
+            // Draw thruster exhaust if thrusters are on. Do it randomly to get a
+            // flicker effect.
+
+            if (!paused && detail && Math.random() < 0.5) {
+                if (KeyControl.up) {
+                    offGraphics.drawPolygon(fwdThruster.sprite);
+                    offGraphics.drawLine(fwdThruster.sprite.xpoints[fwdThruster.sprite.npoints - 1], fwdThruster.sprite.ypoints[fwdThruster.sprite.npoints - 1],
+                            fwdThruster.sprite.xpoints[0], fwdThruster.sprite.ypoints[0]);
+                }
+                if (KeyControl.down) {
+                    offGraphics.drawPolygon(revThruster.sprite);
+                    offGraphics.drawLine(revThruster.sprite.xpoints[revThruster.sprite.npoints - 1], revThruster.sprite.ypoints[revThruster.sprite.npoints - 1],
+                            revThruster.sprite.xpoints[0], revThruster.sprite.ypoints[0]);
+                }
+            }
+        }
+
+        // Draw any explosion debris, counters are used to fade color to black.
+
+        for (i = 0; i < MAX_SCRAP; i++)
+            if (explosions[i].active) {
+                c = (255 / SCRAP_COUNT) * explosionCounter [i];
+                offGraphics.setColor(new Color(c, c, c));
+                offGraphics.drawPolygon(explosions[i].sprite);
+            }
+
+        // Display status and messages.
+
+        offGraphics.setFont(font);
+        offGraphics.setColor(Color.white);
+
+        offGraphics.drawString("Score: " + score, fontWidth, fontHeight);
+        offGraphics.drawString("Ships: " + shipsLeft, fontWidth, d.height - fontHeight);
+        s = "High: " + highScore;
+        offGraphics.drawString(s, d.width - (fontWidth + fm.stringWidth(s)), fontHeight);
+        if (!sound) {
+            s = "Mute";
+            offGraphics.drawString(s, d.width - (fontWidth + fm.stringWidth(s)), d.height - fontHeight);
+        }
+
+        if (!playing) {
+            s = copyName;
+            offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 2 - 2 * fontHeight);
+            s = copyVers;
+            offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 2 - fontHeight);
+            s = copyInfo;
+            offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 2 + fontHeight);
+            s = copyLink;
+            offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 2 + 2 * fontHeight);
+            if (!loaded) {
+                s = "Loading sounds...";
+                w = 4 * fontWidth + fm.stringWidth(s);
+                h = fontHeight;
+                x = (d.width - w) / 2;
+                y = 3 * d.height / 4 - fm.getMaxAscent();
+                offGraphics.setColor(Color.black);
+                offGraphics.fillRect(x, y, w, h);
+                offGraphics.setColor(Color.gray);
+                if (clipTotal > 0)
+                    offGraphics.fillRect(x, y, (int) (w * clipsLoaded / clipTotal), h);
+                offGraphics.setColor(Color.white);
+                offGraphics.drawRect(x, y, w, h);
+                offGraphics.drawString(s, x + 2 * fontWidth, y + fm.getMaxAscent());
+            }
+            else {
+                s = "Game Over";
+                offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 4);
+                s = "'S' to Start";
+                offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 4 + fontHeight);
+            }
+        }
+        else if (paused) {
+            s = "Game Paused";
+            offGraphics.drawString(s, (d.width - fm.stringWidth(s)) / 2, d.height / 4);
+        }
+
+        // Copy the off screen buffer to the screen.
+
+        g.drawImage(offImage, 0, 0, this);
+    }
+
+
+}
